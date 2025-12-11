b8b72d6ade174550bb0609a2233b51333a3cf008
jieunpyo <jieunhye1121@naver.com>
2025-12-11 16:51:35 +0900

    1111

--- src/views/main/HomeView.vue
+++ src/views/main/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-3" v-for="(vo,index) in food_data.list" :key="index">
+      <div class="col-md-3" v-for="(vo, index) in food_data.list" :key="index">
         <div class="thumbnail">
-          <a href="#">
+          <router-link :to="{name:'food_detail',params:{fno:vo.fno}}">
             <img :src="vo.poster" :title="vo.address" style="width:240px;height: 130px">
             <div class="caption">
-              <p>{{vo.name}}</p>
+              <p>{{ vo.name }}</p>
             </div>
-          </a>
+          </router-link>
         </div>
       </div>
     </div>
-    
+    <div class="row text-center" style="margin-top: 20px">
+      <ul class="pagination">
+        <li v-if="food_data.startPage > 1"><a class="a-link" @click="foodListData(food_date.startPage-1)">&laquo;</a></li>
+        <li v-for="i in range(food_data.startPage, food_data.endPage)" :key="i"
+          :class="food_data.curpage == i ? 'active' : ''"><a class="a-link" @click="foodListData(i)">{{ i }}</a></li>
+        <li v-if="food_data.endPage < food_data.totalpage"><a class="a-link" @click="foodListData(food_data.endPage+1)">&raquo;</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
@@ -24,8 +31,8 @@
         | include : App.vue
     home.html = Home.vue
     header.html = Header.vue
-
-    화면 UI
+    
+    화면 UI 
       |
     이벤트 = 메뉴클릭 / 버튼 클릭 
       |
@@ -35,23 +42,34 @@
       |
     해당 vue에서 store에 있는 state를 읽어서 출력
 */
-import { defineComponent,onMounted,computed } from 'vue';
+import { defineComponent, onMounted, computed } from 'vue';
 import { useStore } from 'vuex';
 export default defineComponent({
-  setup(){
-    const store=useStore();
-    const food_data=computed(()=> store.state.foods.food_data)
-    // action함수 호출
-    const foodListData=async(page)=>{
-      await store.dispatch("foods/foodListData",page)
+  setup() {
+    const store = useStore()
+    const food_data = computed(() => store.state.foods.food_data)
+    // action함수 호출 
+    const foodListData = async (page) => {
+      await store.dispatch("foods/foodListData", page)
       console.log(food_data.value)
     }
-    onMounted(()=>{
+    onMounted(() => {
       foodListData(1)
     })
     return {
       food_data,
       foodListData
+    }
+  },
+  methods: {
+    range(start, end) {
+      const len = end - start
+      const arr = []
+      for (let i = 0; i <= len; i++) {
+        arr[i] = start
+        start++
+      }
+      return arr
     }
   }
 
@@ -67,5 +85,8 @@ p {
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
+}
+.a-link:hover{
+  cursor: pointer;
 }
 </style>
